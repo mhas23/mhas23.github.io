58cd56ceb82f296e7c03649bfda4c6b6de67a3de
mhas23 <61035314+mhas23@users.noreply.github.com>
2020-09-20 13:20:21 -0400

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Redirect, HashRouter} from 'react-router-dom';
 import Home from '../Pages/Home'
 import MovieInfo from '../Pages/MovieInfo'
 //import List from '../Pages/List'
